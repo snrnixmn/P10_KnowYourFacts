4a13b86179b14ae8a87a72e615f0a22815cf0b69
18016339 <18016339@myrp.edu.sg>
2020-07-23 22:36:57 +0800

    done

---
+++ src/main/java/com/example/p09knowyourfacts/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.p09knowyourfacts;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
+
+import android.app.Activity;
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Fragment> al;
+    MyFragmentPagerAdapter adapter;
+    ViewPager vPager;
+    Button btnRead;
+    int requestCode = 12345;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        vPager = findViewById(R.id.viewpager1);
+        btnRead = findViewById(R.id.btnReadLater);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+        al = new ArrayList<Fragment>();
+        al.add(new frag1());
+        al.add(new frag2());
+        al.add(new frag3());
+
+        adapter = new MyFragmentPagerAdapter(fm, al);
+
+        vPager.setAdapter(adapter);
+
+        btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar cal = Calendar.getInstance();
+                cal.add(Calendar.SECOND, 5);
+
+                Intent intent = new Intent(MainActivity.this,
+                        ScheduledNReceiver.class);
+
+                PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                        MainActivity.this, requestCode,
+                        intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+                AlarmManager am = (AlarmManager) getSystemService(Activity.ALARM_SERVICE);
+                am.set(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(),
+                        pendingIntent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.action_previous:
+                if (vPager.getCurrentItem() > 0){
+                    int previousPage = vPager.getCurrentItem() - 1;
+                    vPager.setCurrentItem(previousPage, true);
+                }
+                break;
+
+                case R.id.action_random:
+                Random rand = new Random();
+                int total = vPager.getChildCount();
+                vPager.setCurrentItem(rand.nextInt(total),true);
+                break;
+
+                case R.id.action_next:
+                int max = vPager.getChildCount();
+
+                if (vPager.getCurrentItem() < max-1){
+                    int nextPage = vPager.getCurrentItem() + 1;
+                    vPager.setCurrentItem(nextPage, true);
+                }
+
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+}
+
+
---
+++ src/main/java/com/example/p09knowyourfacts/frag3.java
@@ -0,0 +1,45 @@
+package com.example.p09knowyourfacts;
+
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.Random;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class frag3 extends Fragment {
+
+    Button btnChangeColor;
+
+    public frag3() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View view = inflater.inflate(R.layout.fragment_frag3, container,false);
+        btnChangeColor = view.findViewById(R.id.btnChangeColor);
+        btnChangeColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random rnd = new Random();
+                int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+                view.setBackgroundColor(color);
+            }
+        });
+
+        return view;
+    }
+}
